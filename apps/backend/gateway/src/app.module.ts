a55c1edd7525ab930e490af2d63267ec85e156b3
aoda <aoda2826@qq.com>
2025-10-03 20:36:47 +0800

    refactor: Improve code organization and update TypeScript configuration for better compatibility

--- apps/backend/gateway/src/app.module.ts
+++ apps/backend/gateway/src/app.module.ts
@@ -1,44 +1,48 @@
-import path from 'node:path'
-import AuthModule from '@modules/Auth/auth.module'
-import { TripModule } from '@modules/Trip/trip.module'
-import { Module } from '@nestjs/common'
-import { APP_GUARD } from '@nestjs/core'
-import SharedModule from '@shared/shared.module'
-import { DocumentModule } from '@modules/Document/document.module'
-import { EnvConstant } from '@shared/constants/constant'
-import { JwtModule } from '@nestjs/jwt'
-import SignGuard from '@modules/Auth/guards/Sign.guard'
-import JWTGuard from '@modules/Auth/guards/JWT.guard'
+import path from 'node:path';
+
+import { AuthModule } from '@modules/Auth/auth.module';
+import { TripModule } from '@modules/Trip/trip.module';
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import SharedModule from '@shared/shared.module';
+import { DocumentModule } from '@modules/Document/document.module';
+import { EnvConstant } from '@shared/constants/constant';
+import { JwtModule } from '@nestjs/jwt';
+import { SignGuard } from '@modules/Auth/guards/Sign.guard';
+import { JWTGuard } from '@modules/Auth/guards/JWT.guard';
 // import ACLGuard from '@modules/ACL/middlewares/ACL.guard'
-const currentEnv = process.env.NODE_ENV ?? 'uat'
-const configFilePath = path.resolve(__dirname, `./config/${EnvConstant[currentEnv]}/env/index.yaml`)
+const currentEnv = process.env.NODE_ENV ?? 'uat';
+const configFilePath = path.resolve(
+  __dirname,
+  `./config/${EnvConstant[currentEnv]}/env/index.yaml`,
+);
 
 @Module({
-    imports: [
-        SharedModule.forRoot({
-            configFilePath,
-            isIntergrateHttpExceptionFilter: true,
-            isIntergrateHttpInterceptor: true
-        }),
-        JwtModule,
-        TripModule,
-        DocumentModule,
-        AuthModule
-    ],
-    controllers: [],
-    providers: [
-        {
-            provide: APP_GUARD,
-            useClass: SignGuard
-        },
-        {
-            provide: APP_GUARD,
-            useClass: JWTGuard
-        }
-        // {
-        //     provide: APP_GUARD,
-        //     useClass: ACLGuard
-        // }
-    ]
+  imports: [
+    SharedModule.forRoot({
+      configFilePath,
+      isIntergrateHttpExceptionFilter: true,
+      isIntergrateHttpInterceptor: true,
+    }),
+    JwtModule,
+    TripModule,
+    DocumentModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: SignGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JWTGuard,
+    },
+    // {
+    //     provide: APP_GUARD,
+    //     useClass: ACLGuard
+    // }
+  ],
 })
 export class AppModule {}
